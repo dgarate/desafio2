431ea00ad361906ec0cd7aef9482dd909a310f28
dgarate <garatex@gmail.com>
2020-07-11 20:35:06 -0400

    sending the remaining homework

--- desafio2.rb
+++ desafio2.rb
@@ -1,12 +1,35 @@
-a = ['Violeta', 'Andino', 'Clemente','Javiera', 'Paula', 'Pía', 'Ray']
+a = ['Violeta', 'Andino', 'Clemente','Javiera', 'Paula', 'pía', 'ray']
 
-a= a.select do |filter1|
+#desafio #2.1
+z= a.select do |filter1|
     filter1.length> 5
 end
 
-print a
+print z
 
+# desafio #2.2
 b= a.map do |dc|
     dc.downcase
 end
 print b 
+
+# desafio #2.3
+c = a.select do |pnames|
+    pnames.capitalize.start_with?("P")
+end
+
+print c
+
+# desafio #2.4
+d = a.count do |pnames|
+    pnames.capitalize.start_with?("A", "B", "C")
+end
+
+# desafio #2.5
+print d
+
+f = a.map do |pnames|
+    pnames.length
+end
+
+print f
